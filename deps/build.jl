3fff5ae909f556c629edccc69c2b3f7d24787078
Thomas Breloff <tom@breloff.com>
2016-08-15 12:38:03 -0400

    switched build.jl to available/https/build

--- deps/build.jl
+++ deps/build.jl
@@ -1,14 +1,28 @@
 
-function is_installed(pkgstr::AbstractString)
-    try
-        Pkg.installed(pkgstr) === nothing ? false: true
-    catch
-        false
-    end
-end
+# function is_installed(pkgstr::AbstractString)
+#     try
+#         Pkg.installed(pkgstr) === nothing ? false: true
+#     catch
+#         false
+#     end
+# end
 
-for pkgstr in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLDataUtils", "MLMetrics")
-	if !is_installed(pkgstr)
-		Pkg.clone("git@github.com:JuliaML/$pkgstr.jl")
+# for pkgstr in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLDataUtils", "MLMetrics")
+# 	if !is_installed(pkgstr)
+# 		Pkg.clone("https://github.com/JuliaML/$pkgstr.jl")
+# 	end
+# end
+
+for pkg in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLDataUtils", "MLMetrics")
+	try
+		avail = Pkg.available(pkg)
+		if !isempty(avail)
+			# this is a registered package!
+			warn("$pkg is registered in METADATA... it shouldn't be in $(@__FILE__)")
+		end
+	catch
+		# install it
+		Pkg.clone("https://github.com/JuliaML/$pkg.jl")
+		Pkg.build(pkg)
 	end
 end
