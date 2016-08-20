0bfe42349fce9a62428996fe1000f9a5f94c91d8
Thomas Breloff <tom@breloff.com>
2016-08-20 14:16:29 -0400

    move MLDataUtils from build to REQUIRE

--- deps/build.jl
+++ deps/build.jl
@@ -1,19 +1,5 @@
 
-# function is_installed(pkgstr::AbstractString)
-#     try
-#         Pkg.installed(pkgstr) === nothing ? false: true
-#     catch
-#         false
-#     end
-# end
-
-# for pkgstr in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLDataUtils", "MLMetrics")
-# 	if !is_installed(pkgstr)
-# 		Pkg.clone("https://github.com/JuliaML/$pkgstr.jl")
-# 	end
-# end
-
-for pkg in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLDataUtils", "MLMetrics")
+for pkg in ("LearnBase", "Transformations", "ObjectiveFunctions", "Losses", "StochasticOptimization", "MLMetrics")
 	try
 		avail = Pkg.available(pkg)
 		if !isempty(avail)
